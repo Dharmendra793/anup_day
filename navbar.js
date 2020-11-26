cb33f4f71268a92e17aaefd39811c89f1006b0be
Dharmendra793 <dharam23dhruve@gmail.com>
2020-11-26 13:26:21 +0530

    adding a navbar

---
+++ navbar.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector(".menu-icon");
+
+menuIcon.addEventListener("click",() => {
+    let menu = document.querySelector(".menu-container");
+
+    menu.classList.toggle("active")
+
+})
